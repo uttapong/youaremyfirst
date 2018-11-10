77240da1400c0308862d3f2f42898d9714c87431
Uttapong Ruangrit <uttapong@gmail.com>
2018-11-10 16:25:52 +0700

    update thai date

--- plugins/uttapong/s3videouploader/models/VideoUploader.php
+++ plugins/uttapong/s3videouploader/models/VideoUploader.php
@@ -20,4 +20,21 @@ class VideoUploader extends Model
      */
     public $rules = [
     ];
+
+    protected $appends = array('thaidate');
+
+    public function getThaidateAttribute(){
+        $strDate=$this->created_at;
+		$strYear = date("Y",strtotime($strDate))+543;
+		$strMonth= date("n",strtotime($strDate));
+		$strDay= date("j",strtotime($strDate));
+		$strHour= date("H",strtotime($strDate));
+		$strMinute= date("i",strtotime($strDate));
+		$strSeconds= date("s",strtotime($strDate));
+		$strMonthCut = Array("","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","มิ.ย.","ก.ค.","ส.ค.","ก.ย.","ต.ค.","พ.ย.","ธ.ค.");
+		$strMonthThai=$strMonthCut[$strMonth];
+		return "$strDay $strMonthThai $strYear";
+		// return "$strDay $strMonthThai $strYear, $strHour:$strMinute";
+	
+    }
 }
